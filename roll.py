2c6a5cf54873897d8c6fae2a5fe82583f08b735c
Sean <sean@zipcodes-MBP.fios-router.home>
2021-07-07 21:03:30 -0400

    final commit

--- roll.py
+++ roll.py
@@ -5,3 +5,18 @@
 # roll 3 - produce three numbers and print them.
 #
 
+import random 
+
+def roll_dice():
+    while True:
+        decision = int(input('Please choose\n1. Roll Dice\n2. Exit\n'))
+        if decision == 2:
+            break
+        elif decision == 1:
+            numDice = int(input(('Please enter the number of dice to roll\n')))
+            for i in range(numDice):
+                print('Dice Roll ' + str(i + 1) + ': ' + str(random.randint(1, 6)))
+        else:
+            print("That was not a correction selection. Please Try again\n")
+
+roll_dice()
